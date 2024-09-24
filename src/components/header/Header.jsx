e24ff52cf4ee98b99bee0f0e1c7d77973cee09db
byewhile <93428940+byewhile@users.noreply.github.com>
2024-09-24 22:19:10 +0300

    update header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import styles from "./header.module.css";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-        <h1><Link href="/">byewhile</Link></h1>
+        <h1>
+          <Link href="/">
+            <Image src="/favicon.ico" width="32" height="32" alt="icon" />
+            byewhile
+          </Link>
+        </h1>
     </header>
   )
 }
