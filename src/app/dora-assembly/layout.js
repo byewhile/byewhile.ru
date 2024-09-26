9cd440dfcaf9066882187fadc123bdd47f5944e9
byewhile <93428940+byewhile@users.noreply.github.com>
2024-09-26 19:27:43 +0300

    add layouts for pages

---
+++ src/app/dora-assembly/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Сборка Доры",
+    description: "Игры и прочие странности. Сборка Доры.",
+}
+
+export default function ClientLayout({ children }) {
+    return children
+}
---
+++ src/app/find-emoji/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Найди emoji",
+    description: "Игры и прочие странности. Найди emoji.",
+}
+
+export default function ClientLayout({ children }) {
+    return children
+}
