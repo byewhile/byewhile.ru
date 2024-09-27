a93ac992656e7c85109b2bcca94e82da29e6d0ce
byewhile <93428940+byewhile@users.noreply.github.com>
2024-09-27 16:16:50 +0300

    update 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,24 @@
 "use client"
 
-import { useEffect } from "react"
-import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react";
+import styles from "./not_found.module.css";
 
 export default function not_found() {
-    const router = useRouter();
+    const [warning, setWarning] = useState("Возвращайтесь немедленно!");
+    const warnings = ["Возвращайтесь немедленно!", "Return immediately!", "¡Vuelve inmediatamente!", "Volte imediatamente!", "Komm sofort wieder!", "Revenez immédiatement!", "赶紧回来吧!", "すぐに戻ってきてください!", "즉시 돌아오세요!", "أعود على الفور!"];
+
     useEffect(() => {
-        router.replace("/")
-    }, [])
+        setTimeout(() => {
+            let index = warnings.indexOf(warning);
+            if (index == 9) index = -1
+            setWarning(warnings[index + 1]);
+        }, 500)
+    }, [, warning])
+
+    return (
+        <main className={styles.not_foundPage}>
+            <h2>404</h2>
+            <p>{warning}</p>
+        </main>
+    )
 }
